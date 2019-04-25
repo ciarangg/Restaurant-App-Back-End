e76186e15eaf14586bd238d26904615287231ef5
Ciaran <cguhagil@terpmail.umd.edu>
2019-04-25 11:31:31 -0600

    added cors

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const menu = require('./routes/menu.route'); // Imports routes for the menu
+const cors = require('cors');
+
+app.use(cors())
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://ciarangg:Sloightwerk2@ds111940.mlab.com:11940/restaurant';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
